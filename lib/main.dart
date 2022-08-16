96b840659f321863b988186ab37c2252dac11032
KhilfiNazarudin <201945D@mymail.nyp.edu.sg>
2022-08-16 13:05:34 +0800

    Scaffolding

--- lib/main.dart
+++ lib/main.dart
@@ -103,142 +103,144 @@ class _loginScreenState extends State<loginScreen> {
           provider.setUser(snapshot.data[0], snapshot.data[1]);
           return mainScreen();
         } else {
-          return Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [defaultp, defaulta],
-              )),
-              child: Center(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15),
-                        child: Container(
-                          child: Text(
-                            "Login to your account",
-                            style: TextStyle(
-                                fontSize: 80,
-                                color: Colors.amber,
-                                fontFamily: 'NexaBold',
-                                fontWeight: FontWeight.w600),
+          return Scaffold(
+            body: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [defaultp, defaulta],
+                )),
+                child: Center(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Container(
+                            child: Text(
+                              "Login to your account",
+                              style: TextStyle(
+                                  fontSize: 80,
+                                  color: Colors.amber,
+                                  fontFamily: 'NexaBold',
+                                  fontWeight: FontWeight.w600),
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.width * 0.08,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: 50, right: 50, bottom: 10),
-                        child: TextField(
-                          controller: emailController,
-                          decoration: InputDecoration(
-                            hintText: 'Username',
-                            hintStyle: TextStyle(
-                                fontFamily: 'NexaBold',
-                                fontWeight: FontWeight.w500,
-                                color: Colors.white),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.width * 0.08,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 50, right: 50, bottom: 10),
+                          child: TextField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              hintText: 'Username',
+                              hintStyle: TextStyle(
+                                  fontFamily: 'NexaBold',
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              contentPadding: const EdgeInsets.all(20),
                             ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                            contentPadding: const EdgeInsets.all(20),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: 50, right: 50, bottom: 10),
-                        child: TextField(
-                          controller: passwordController,
-                          decoration: InputDecoration(
-                            hintText: 'Password',
-                            hintStyle: TextStyle(
-                                fontFamily: 'NexaBold',
-                                fontWeight: FontWeight.w500,
-                                color: Colors.white),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 50, right: 50, bottom: 10),
+                          child: TextField(
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              hintStyle: TextStyle(
+                                  fontFamily: 'NexaBold',
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              contentPadding: const EdgeInsets.all(20),
                             ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                            contentPadding: const EdgeInsets.all(20),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.width * 0.08,
-                      ),
-                      Text(
-                        message,
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'NexaBold',
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.all(30),
-                          child: GestureDetector(
-                              onTap: () async {
-                                var password = passwordController.text.trim();
-                                var reguser =
-                                    await FirebaseAuthService().signIn(
-                                  email: emailController.text.trim(),
-                                  password: passwordController.text.trim(),
-                                );
-
-                                if (reguser != null) {
-                                  Reference ref = FirebaseStorage.instance
-                                      .ref()
-                                      .child("/user/profile/" + reguser.uid);
-                                  var url = await ref.getDownloadURL();
-                                  print(url);
-                                  provider.setUser(reguser, url);
-                                  save(reguser,
-                                      password); // to shared preference
-                                  Navigator.of(context).pushReplacement(
-                                      MaterialPageRoute(
-                                          builder: (context) => mainScreen()));
-                                }
-                                setState(() {
-                                  idinput = emailController.text;
-                                  pwdinput = passwordController.text;
-                                  emailController.clear();
-                                  passwordController.clear();
-                                });
-                              },
-                              child: Container(
-                                  width: 200,
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      border: Border.all(
-                                        color: Colors.white,
-                                        width: 1,
-                                      )),
-                                  child: Align(
-                                    alignment: Alignment.center,
-                                    child: Text(
-                                      "Login",
-                                      style: TextStyle(
+                        SizedBox(
+                          height: MediaQuery.of(context).size.width * 0.08,
+                        ),
+                        Text(
+                          message,
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'NexaBold',
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.all(30),
+                            child: GestureDetector(
+                                onTap: () async {
+                                  var password = passwordController.text.trim();
+                                  var reguser =
+                                      await FirebaseAuthService().signIn(
+                                    email: emailController.text.trim(),
+                                    password: passwordController.text.trim(),
+                                  );
+          
+                                  if (reguser != null) {
+                                    Reference ref = FirebaseStorage.instance
+                                        .ref()
+                                        .child("/user/profile/" + reguser.uid);
+                                    var url = await ref.getDownloadURL();
+                                    print(url);
+                                    provider.setUser(reguser, url);
+                                    save(reguser,
+                                        password); // to shared preference
+                                    Navigator.of(context).pushReplacement(
+                                        MaterialPageRoute(
+                                            builder: (context) => mainScreen()));
+                                  }
+                                  setState(() {
+                                    idinput = emailController.text;
+                                    pwdinput = passwordController.text;
+                                    emailController.clear();
+                                    passwordController.clear();
+                                  });
+                                },
+                                child: Container(
+                                    width: 200,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        border: Border.all(
                                           color: Colors.white,
-                                          fontSize: 25,
-                                          fontFamily: 'NexaBold',
-                                          fontWeight: FontWeight.w500),
-                                    ),
-                                  ))))
-                    ],
+                                          width: 1,
+                                        )),
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        "Login",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 25,
+                                            fontFamily: 'NexaBold',
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                    ))))
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            
+              
+            ),
           );
         }
       },
